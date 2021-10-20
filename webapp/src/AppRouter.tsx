37cb65b18de6fdd42442a8050f12dc2e302b4f2d
weilingxie1 <sqlnever@gmail.com>
2021-10-21 10:59:11 +1300

    Added eslint and prettier

--- webapp/src/AppRouter.tsx
+++ webapp/src/AppRouter.tsx
@@ -3,19 +3,19 @@ import Menu from './components/Menu/Menu'
 import RegisterCardForm from './components/RegisterCardForm/RegisterCardForm'
 
 const AppRouter: React.FC = () => {
-    return(
-        <Switch>
-            <Route path="/RegisterCardForm" exact>
-            <RegisterCardForm />
-            </Route>
-            <Route path="/Menu" exact>
-            <Menu />          
-            </Route>
-            <Route path="/">
-            <Redirect to="/RegisterCardForm" />
-            </Route>
-        </Switch>
-    )  
+  return (
+    <Switch>
+      <Route path="/RegisterCardForm" exact>
+        <RegisterCardForm />
+      </Route>
+      <Route path="/Menu" exact>
+        <Menu />
+      </Route>
+      <Route path="/">
+        <Redirect to="/RegisterCardForm" />
+      </Route>
+    </Switch>
+  )
 }
 
 export default AppRouter
