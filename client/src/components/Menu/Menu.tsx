4eaacef2407615ac03aedb74010fad93c3107842
weilingxie1 <sqlnever@gmail.com>
2021-10-20 13:59:14 +1300

    Added Header to Menu

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import Header from '../Header/Header'
+import { useHistory } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+
+const MenuTitle = 'Menu'
+const RegisterCardFormPath = '/RegisterCardForm'
+const MenuTextContent = 'This is menu content'
+const HeaderMenuIcon = <MenuIcon fontSize="large" /> 
 
 const Menu: React.FC = () => {
+    const history = useHistory()
+
+    const switchToRegisterCardForm = ():void => {
+        history.push(RegisterCardFormPath)
+    }
 
     return (
-    <div data-testid="Menu">
-        Menu
+    <div data-testid="Menu" style={{ height: '100%'}}>
+        <Header
+            title = {MenuTitle}
+            headerIcon = {HeaderMenuIcon}
+            onClickHeaderButton = {switchToRegisterCardForm}
+        />
+        <Card
+            sx={{
+                padding: '10px'
+            }}
+        >
+            <Grid container 
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                sx={{ height: '100%' }}
+            >
+                <Typography variant="h5" sx={{ height: '100%', textAlign: 'center' }}>
+                    {MenuTextContent}
+                </Typography>
+            </Grid>
+        </Card>
     </div>
     )
 }
