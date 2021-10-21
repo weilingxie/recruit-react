1a5587106f22c7bd6a6219b83678075b067a4438
weilingxie1 <sqlnever@gmail.com>
2021-10-21 14:18:28 +1300

    Reformated tests

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react'
+import App from './App'
 
 describe('App', () => {
   it('renders RegisterCardContainer', () => {
     const { getByTestId } = render(<App />)
-    expect(getByTestId('RegisterCardContainer')).toBeTruthy();
+    expect(getByTestId('RegisterCardContainer')).toBeTruthy()
   })
 })
--- webapp/src/components/Header/Header.test.tsx
+++ webapp/src/components/Header/Header.test.tsx
@@ -4,35 +4,27 @@ import { createMemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 
 const title = 'TestTitle'
-const headerIcon = <div data-testid='testHeaderIcon'></div> 
+const headerIcon = <div data-testid="testHeaderIcon"></div>
 const toPath = '/'
 
 describe('Header Tests', () => {
-    it('Should render title and icon', () => {
-        const { getByTestId } = render(
-            <Header 
-                title = {title}
-                headerIcon = {headerIcon}
-                toPath = {toPath}
-            />
-        )
-        expect(screen.getByText(title)).toBeTruthy()
-        expect(getByTestId('testHeaderIcon')).toBeTruthy()
-    })
+  it('Should render title and icon', () => {
+    const { getByTestId } = render(
+      <Header title={title} headerIcon={headerIcon} toPath={toPath} />
+    )
+    expect(screen.getByText(title)).toBeTruthy()
+    expect(getByTestId('testHeaderIcon')).toBeTruthy()
+  })
 
-    it('should be redirected to toPath when button is clicked', () => {        
-        const history = createMemoryHistory()
-        const { getByRole } = render(
-            <Router history={history}>
-                <Header 
-                title = {title}
-                headerIcon = {headerIcon}
-                toPath = {toPath}
-                />
-            </Router>        
-        )
-        fireEvent.click(getByRole('button'))        
-    
-        expect(window.location.pathname).toEqual(toPath)
-    })    
+  it('should be redirected to toPath when button is clicked', () => {
+    const history = createMemoryHistory()
+    const { getByRole } = render(
+      <Router history={history}>
+        <Header title={title} headerIcon={headerIcon} toPath={toPath} />
+      </Router>
+    )
+    fireEvent.click(getByRole('button'))
+
+    expect(window.location.pathname).toEqual(toPath)
+  })
 })
