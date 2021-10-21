a7d51ae2eee3d5e1aa05ef8202e1605e59930e95
weilingxie1 <sqlnever@gmail.com>
2021-10-21 14:18:28 +1300

    Reformated tests

--- webapp/src/components/RegisterCardContainer/RegisterCardContainer.test.tsx
+++ webapp/src/components/RegisterCardContainer/RegisterCardContainer.test.tsx
@@ -5,24 +5,24 @@ import { createMemoryHistory, History } from 'history'
 import { ComponentInfoList } from '../../constans/ComponentInfoConstant'
 
 describe('RegisterCardContainer Tests', () => {
+  const history = createMemoryHistory()
+  const renderRegisterCardContainer = (history: History) => {
+    return render(
+      <Router history={history}>
+        <RegisterCardContainer componentInfoList={ComponentInfoList} />
+      </Router>
+    )
+  }
+
+  it('should render Header', () => {
+    const { getByTestId } = renderRegisterCardContainer(history)
+    expect(getByTestId('Header')).toBeTruthy()
+  })
+
+  it('should render title of componentInfo', () => {
     const history = createMemoryHistory()
-    const renderRegisterCardContainer = (history: History)  => {
-        return render(
-            <Router history={history}>
-                <RegisterCardContainer componentInfoList={ComponentInfoList} />
-            </Router>
-        )
-    }
-
-    it('should render Header', () => {        
-            const { getByTestId } = renderRegisterCardContainer(history)
-            expect(getByTestId('Header')).toBeTruthy()
-    })    
-
-    it('should render title of componentInfo', () => {        
-        const history = createMemoryHistory()
-        history.push(ComponentInfoList[0].Path)
-        const { getByText } = renderRegisterCardContainer(history)    
-        expect(getByText(ComponentInfoList[0].Title)).toBeTruthy()
-    })    
+    history.push(ComponentInfoList[0].Path)
+    const { getByText } = renderRegisterCardContainer(history)
+    expect(getByText(ComponentInfoList[0].Title)).toBeTruthy()
+  })
 })
