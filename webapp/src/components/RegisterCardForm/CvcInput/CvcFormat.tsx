cf7eafc6b150287bc301359d2365504a1729f615
weilingxie1 <sqlnever@gmail.com>
2021-10-23 16:45:41 +1300

    Added CvcInput

---
+++ webapp/src/components/RegisterCardForm/CvcInput/CvcFormat.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import NumberFormat from 'react-number-format'
+
+type CvcFormatProps = {
+  onChange: (event: { target: { name: string; value: string } }) => void
+  name: string
+}
+
+const CvcFormat = React.forwardRef<NumberFormat, CvcFormatProps>(
+  function NumberFormatCustom(props, ref) {
+    const { onChange, ...other } = props
+
+    return (
+      <NumberFormat
+        {...other}
+        data-testid="CvcFormat"
+        getInputRef={ref}
+        onValueChange={(values) => {
+          onChange({
+            target: {
+              name: props.name,
+              value: values.value,
+            },
+          })
+        }}
+        type="password"
+        format="###"
+      />
+    )
+  }
+)
+
+export default CvcFormat
--- webapp/src/components/RegisterCardForm/ValidateCreditCard.ts
+++ webapp/src/components/RegisterCardForm/ValidateCreditCard.ts
@@ -2,16 +2,13 @@ const ValidateCardNumber = (cardNumber: string): string =>
   cardNumber.length !== 16 ? 'Credit card number is invalid' : ''
 
 const ValidateExpireDate = (expireDate: string): string => {
-  console.log(expireDate)
-  const errorMessage = 'Expire date number is invalid'
-  try {
-    const regex = /^(0[1-9]|1[0-2])([0-9]{2})$/
-    return expireDate.length !== 4 || !expireDate.match(regex)
-      ? errorMessage
-      : ''
-  } catch (e) {
-    return errorMessage
-  }
+  const regex = /^(0[1-9]|1[0-2])([0-9]{2})$/
+  return expireDate.length !== 4 || !expireDate.match(regex)
+    ? 'Expire date number is invalid'
+    : ''
 }
 
-export { ValidateCardNumber, ValidateExpireDate }
+const ValidateCvc = (cvc: string): string =>
+  cvc.length !== 3 ? 'Cvc is invalid' : ''
+
+export { ValidateCardNumber, ValidateExpireDate, ValidateCvc }
