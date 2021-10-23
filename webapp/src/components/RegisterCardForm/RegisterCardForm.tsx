0c771cfc2e9d908b0d840a7fff39bc19c23cd689
weilingxie1 <sqlnever@gmail.com>
2021-10-23 22:44:50 +1300

    Tuned the style of Register card form

--- webapp/src/components/RegisterCardForm/RegisterCardForm.tsx
+++ webapp/src/components/RegisterCardForm/RegisterCardForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
 import CardNumberInput from './CardNumberInput/CardNumberInput'
 import ExpireDateInput from './ExpireDateInput/ExpireDateInput'
 import CvcInput from './CvcInput/CvcInput'
@@ -70,48 +71,81 @@ const RegisterCardForm: React.FC<RegisterCardFormProps> = ({
 
   return (
     <form data-testid="RegisterCardForm" onSubmit={onSubmit}>
-      <Grid container item sm={12} md={10} lg={6} spacing={2}>
-        <Grid item>
-          <CardNumberInput
-            onCardNumberChange={onCardNumberChange}
-            cardNumber={cardNumber}
-            error={getErrors().some((err) => err.name === 'cardNumber')}
-            helperText={
-              getErrors().find((err) => err.name === 'cardNumber')
-                ?.errorMessage || ''
-            }
-          />
-        </Grid>
-        <Grid item>
-          <ExpireDateInput
-            onExpireDateChange={onExpireDateChange}
-            expireDate={expireDate}
-            error={getErrors().some((err) => err.name === 'expireDate')}
-            helperText={
-              getErrors().find((err) => err.name === 'expireDate')
-                ?.errorMessage || ''
-            }
-          />
-        </Grid>
-        <Grid item>
-          <CvcInput
-            onCvcChange={onCvcChange}
-            cvc={cvc}
-            error={getErrors().some((err) => err.name === 'cvc')}
-            helperText={
-              getErrors().find((err) => err.name === 'cvc')?.errorMessage || ''
-            }
-          />
-        </Grid>
-        <Grid item sm={12} lg={12}>
-          <Button
-            variant="contained"
-            color="success"
-            type="submit"
-            disabled={getErrors().length > 0}
-          >
-            Submit
-          </Button>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid container item xs={12} sm={8} md={6} lg={4} xl={4}>
+          <Grid item container justifyContent="start" spacing={2}>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <Typography variant="h5">Welcome, William</Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <CardNumberInput
+                onCardNumberChange={onCardNumberChange}
+                cardNumber={cardNumber}
+                error={getErrors().some((err) => err.name === 'cardNumber')}
+                helperText={
+                  getErrors().find((err) => err.name === 'cardNumber')
+                    ?.errorMessage || ''
+                }
+              />
+            </Grid>
+            <Grid
+              item
+              container
+              justifyContent="space-between"
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+              spacing={2}
+            >
+              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+                <ExpireDateInput
+                  onExpireDateChange={onExpireDateChange}
+                  expireDate={expireDate}
+                  error={getErrors().some((err) => err.name === 'expireDate')}
+                  helperText={
+                    getErrors().find((err) => err.name === 'expireDate')
+                      ?.errorMessage || ''
+                  }
+                />
+              </Grid>
+              <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
+                <CvcInput
+                  onCvcChange={onCvcChange}
+                  cvc={cvc}
+                  error={getErrors().some((err) => err.name === 'cvc')}
+                  helperText={
+                    getErrors().find((err) => err.name === 'cvc')
+                      ?.errorMessage || ''
+                  }
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              container
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+              spacing={2}
+            >
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="success"
+                  type="submit"
+                  fullWidth={true}
+                  size="medium"
+                  disabled={getErrors().length > 0}
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </form>
