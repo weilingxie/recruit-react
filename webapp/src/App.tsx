436b0ca57dda2ab69b1924b3403c9eb2a81d8d33
weilingxie1 <sqlnever@gmail.com>
2021-10-25 12:02:16 +1300

    Extract 2 contexts to ContextContainer

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,32 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './AppRouter'
 import RegisterCardContainer from './components/RegisterCardContainer/RegisterCardContainer'
 import ComponentInfoList from './constants/ComponentInfoConstant'
-import StyleContext from './contexts/StyleContext'
-import UserContext from './contexts/UserContext'
-import { IStyle, IUser } from './types/Types'
-
-const user: IUser = {
-  firstName: 'William',
-  lastName: 'Xie',
-}
-
-const style: IStyle = {
-  input: {
-    fontSize: '1.2em',
-    variant: 'filled',
-  },
-}
+import ContextContainer from './contexts/ContextContainer'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <StyleContext.Provider value={style}>
-        <UserContext.Provider value={user}>
-          <RegisterCardContainer componentInfoList={ComponentInfoList}>
-            <AppRouter />
-          </RegisterCardContainer>
-        </UserContext.Provider>
-      </StyleContext.Provider>
+      <ContextContainer>
+        <RegisterCardContainer componentInfoList={ComponentInfoList}>
+          <AppRouter />
+        </RegisterCardContainer>
+      </ContextContainer>
     </BrowserRouter>
   )
 }
---
+++ webapp/src/contexts/ContextContainer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { IStyle, IUser } from '../types/Types'
+import StyleContext from './StyleContext'
+import UserContext from './UserContext'
+
+const style: IStyle = {
+  input: {
+    fontSize: '1.2em',
+    variant: 'filled',
+  },
+}
+
+const user: IUser = {
+  firstName: 'William',
+  lastName: 'Xie',
+}
+
+type ContextContainerProps = {
+  children?: React.ReactNode
+}
+
+const ContextContainer: React.FC<ContextContainerProps> = ({
+  children,
+}: ContextContainerProps) => (
+  <StyleContext.Provider value={style}>
+    <UserContext.Provider value={user}>{children}</UserContext.Provider>
+  </StyleContext.Provider>
+)
+
+export default ContextContainer
