94ed58cd7079bf200411679211dcb7ec39cda9e8
weilingxie1 <sqlnever@gmail.com>
2021-10-25 11:21:35 +1300

    Added UserContext

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,13 +3,22 @@ import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './AppRouter'
 import RegisterCardContainer from './components/RegisterCardContainer/RegisterCardContainer'
 import ComponentInfoList from './constants/ComponentInfoConstant'
+import UserContext from './contexts/UserContext'
+import { IUser } from './types/Types'
+
+const user: IUser = {
+  firstName: 'William',
+  lastName: 'Xie',
+}
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <RegisterCardContainer componentInfoList={ComponentInfoList}>
-        <AppRouter />
-      </RegisterCardContainer>
+      <UserContext.Provider value={user}>
+        <RegisterCardContainer componentInfoList={ComponentInfoList}>
+          <AppRouter />
+        </RegisterCardContainer>
+      </UserContext.Provider>
     </BrowserRouter>
   )
 }
