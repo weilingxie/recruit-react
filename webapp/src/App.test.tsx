e007966ad4510254138281c8cf11d7702ef32aa8
weilingxie1 <sqlnever@gmail.com>
2021-10-26 21:02:19 +1300

    Added 1 test to App

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -2,8 +2,13 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 describe('App tests', () => {
-  it('Should render RegisterCardContainer by default', () => {
+  it('Should render RegisterCardContainer', () => {
     const { getByTestId } = render(<App />)
     expect(getByTestId('RegisterCardContainer')).toBeTruthy()
+  })
+
+  it('Should render RegisterCardForm by default', () => {
+    const { getByTestId } = render(<App />)
+    expect(getByTestId('RegisterCardForm')).toBeTruthy()
   })
 })
